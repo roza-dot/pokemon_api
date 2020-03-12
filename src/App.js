fcad3a7d841675eea8ad9964eaed7eaec48de734
Roza Grigorkiv <roza_grigorkiv@yahoo.com>
2020-03-12 12:41:32 -0600

    pokemon api project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from "axios";
+
+
 
 function App() {
+  const [pokemon,setPokemon] = useState([]);
+  const handleClick = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+    .then(result => {
+      setPokemon(result.data.results)
+        console.log(result)
+    }
+    )}
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Fetch Pokemon</button>
+      <div>
+        {pokemon.map((pokemon, i)=>(
+          <p key = {i}>{pokemon.name}</p>
+        ))}
+      </div>
     </div>
   );
 }
